f178503796860ffb894079b249a9af3f35f776cc
Michel Bretschneider <embix@gmx.net>
2014-06-15 21:00:58 +0200

    define basic grammar to parse first two properties...

---
+++ Irony.Samples/DataGrammars/Hoi2eugGrammar.cs
@@ -0,0 +1,37 @@
+﻿using Irony.Parsing;
+
+namespace Irony.Samples.DataGrammars
+{
+    /// <summary>
+    /// This class defines the Grammar for the Hoi2 eug file format.
+    /// </summary>
+    [Language("Hoi2eug", "0.1.0", "Hoi2 savegame/scenario grammar")]
+    public class Hoi2eugGrammar : Grammar
+    {
+        public Hoi2eugGrammar()
+        {
+
+            // terminals
+            var hstring = new StringLiteral("String", "\"");
+            var hnumber = new NumberLiteral("Number");
+            var name = new IdentifierTerminal("Name");
+
+            // non-terminals
+            var scenario = new NonTerminal("Scenario");
+            var property = new NonTerminal("Property");
+            var propertyName = new NonTerminal("PropertyName");
+            var propertyValue = new NonTerminal("PropertyValue");
+            var hvalue = new NonTerminal("Value");
+
+            scenario.Rule = property + property;
+
+            property.Rule = propertyName + "=" + propertyValue;
+            propertyName.Rule = name;
+            propertyValue.Rule = hvalue;
+            hvalue.Rule = name | hnumber | hstring;
+
+            this.Root = scenario;
+            MarkPunctuation("{", "}", "=");
+        }
+    }
+}
